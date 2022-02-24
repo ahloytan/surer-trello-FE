36b463e8865dbece0a528a640bc41b161472e470
Aloysius <chewinggum222@hotmail.com>
2022-02-24 22:06:04 +0800

    create project page

---
+++ src/components/Projects/ProjectModal.vue
@@ -0,0 +1,147 @@
+<template>
+  <b-modal v-model="modalSub" :width="640">
+    <div class="card">
+			<div class="card-content has-text-left">
+				<div class="create-project"
+					v-if="isNewProject">
+					<h2 class="is-size-6 has-text-weight-bold">Create New Project</h2>
+					<div class="task-description mt-3">
+						<b-field
+							v-bind:type="createProjectMsgType"
+							v-bind:message="createProjectMsg">
+							<b-input
+								v-model="projectDescription"
+								type="textarea"
+								required
+								placeholder="Enter Project Name"
+								maxlength="25"
+								rows="2"
+								>
+							</b-input>
+						</b-field>
+					</div>
+
+					<div class="btns">
+						<b-button
+							type="is-link is-success"
+							@click="projectDescription !== '' ? ($emit('close'), createProject(projectDescription),projectDescription='') : (createProjectMsg='Enter project name', createProjectMsgType='is-danger')">
+							Create
+						</b-button>
+					</div>
+
+				</div>
+
+				<div class="create-project"
+					v-else>
+					<h2 class="is-size-6 has-text-weight-bold">Join Project Team</h2>
+					<div class="task-description mt-3">
+						<b-field
+							v-bind:type="joinTeamMsgType"
+							v-bind:message="joinTeamMsg">
+							<b-input
+								v-model="projectId"
+								type="number"
+								required
+								rows="1"
+								placeholder="Enter Project ID">
+							</b-input>
+						</b-field>
+					</div>
+
+					<div class="btns">
+						<b-button
+							type="is-link is-success"
+							@click="checkProjectId() ? (joinTeam(projectId),projectId='',$emit('close')) : (joinTeamMsg='Invalid Project ID', joinTeamMsgType='is-danger')">
+							Join Team
+						</b-button>
+					</div>
+
+				</div>
+			</div>
+		</div>
+	</b-modal>
+</template>
+
+<script>
+	export default {
+		name: 'ProjectModal',
+		props: {
+			availableProjects: Array,
+			isModalActive: Boolean,
+			isNewProject: Boolean,
+			task: Object,
+			members: Array,
+		},
+		data() {
+			return {
+				projectDescription: "",
+				projectId: "",
+				joinTeamMsg: "",
+				joinTeamMsgType: "",
+				createProjectMsg: "",
+				createProjectMsgType: "",
+				resetEmpty: "",
+			}
+		},
+		computed: {
+      //https://stackoverflow.com/questions/40574661/avoid-mutating-a-prop-directly-since-the-value-will-be-overwritten
+      modalSub:{
+        get: function(){
+          return this.isModalActive
+        },
+        set: function(){
+          this.$emit('close', "joinTeamMsg='';createProjectMsg='';createProjectMsgType='';joinTeamMsgType='';joinTeamMsg='';resetEmpty='' ")
+        }
+      }
+		},
+		methods: {
+			createProject(description) {
+				this.$parent.createProject(description)
+				this.createProjectMsg = "",
+				this.createProjectType = "",
+				this.projectDescription = ""
+			},
+			joinTeam(projectId){
+				this.$parent.joinTeam(projectId)
+			},
+			checkProjectId(){
+				this.joinTeamMsg=''
+				this.joinTeamMsgType=''
+				let valid = this.availableProjects.includes(parseInt(this.projectId))
+				if (!valid) {this.projectId = ''}
+				return valid
+			}
+		},
+		mounted() {
+		}
+	}
+</script>
+
+<style lang="scss">
+  @import '@/assets/scss/_variables.scss';
+
+	.card {
+		margin: 0 15px;
+	}
+
+	.avatar {
+    width: 30px;
+    height: 30px;
+		padding: 6px;
+		color: $white;
+		cursor: pointer;
+		font-weight: bold;
+		margin-right: 3px;
+		text-align: center;
+		border-radius: 30px;
+    display: inline-block;
+    background: $dark-green;
+		text-transform: uppercase;
+	}
+
+	.btns {
+		margin-top: 10px;
+		text-align: right;
+	}
+
+</style>
---
+++ src/components/Projects/ProjectRow.vue
@@ -0,0 +1,75 @@
+<template>
+  <div class="project-container container-fluid" @redirect="this.$emit('click')">
+    <div class="header m-3">
+      <div class="columns">
+        <div class="column">
+          <div class="right">
+            <h2 class="is-size-6 has-text-weight-bold">Project: {{ description }} </h2>
+          </div>
+        </div>
+        <div class="column">
+          <div class="right">
+            <h2 class="is-size-6 has-text-weight-bold">Last Modified At: {{ last_modified }}</h2>
+          </div>
+        </div>
+        <div class="column">
+          <div class="avatar rounded-circle is-size-7"
+            v-for="person in members"
+            v-bind:key="'avatar-'+ person.fname"
+            v-bind:title="person.fname + ' ' + person.lname + ' '"
+            >
+            {{person.fname[0]}}{{person.lname[0]}}
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: 'ProjectRow',
+  props: {
+    description: String,
+    last_modified: String,
+    members: Array,
+  },
+  data() {
+    return {
+
+    }
+  },
+  methods: {
+  }
+}
+</script>
+
+<style lang="scss">
+  @import '@/assets/scss/_variables.scss';
+
+  .project-container {
+    height: 170px;
+    margin: auto;
+    padding: 10px;
+    max-width: max;
+    cursor: pointer;
+    border-radius:5px;
+    background-color: $grey-light;
+
+    @include breakpoint(tablet2){
+      height: 75px;
+		}
+  }
+
+  .right {
+    text-align: center;
+
+    @include breakpoint(tablet2){
+      text-align: start;
+		}
+  }
+  
+
+</style>
